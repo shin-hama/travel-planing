822deed8f8a79ba6ffaa0f9ad61091e25da5a061
shin-hama <htysivista@gmail.com>
2022-02-04 21:06:55 +0900

    FEAT: Display google map test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
+
+import './App.css'
+
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+}
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LoadScript
+      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY || ''}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default App;
+export default App
