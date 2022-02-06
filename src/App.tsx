edca16dc1137c749bbbd4e52b35f947097c3be7a
shin-hama <htysivista@gmail.com>
2022-02-07 08:50:03 +0900

    FEAT: Remove css

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import { Map } from 'components/atoms'
 
-import './App.css'
 import { SelectedPrefectureProvider } from 'contexts/SelectedPrefectureProvider'
 import { ApolloClientProvider } from 'contexts/ApolloClientProvider'
 import FeaturedPlaces from 'components/FeaturedPlaces'
 
 function App() {
   return (
-    <>
       <ApolloClientProvider>
         <SelectedPrefectureProvider>
           <FeaturedPlaces />
           <Map />
         </SelectedPrefectureProvider>
       </ApolloClientProvider>
-    </>
   )
 }
 
