3d488ccecea48a35e5d302623cacb40bde58c053
shamada <shamada@jeol.co.jp>
2022-03-03 16:42:37 +0900

    WIP: Optimize route, but wait to add prefectures place_id on database

--- src/components/RouteViewer.tsx
+++ src/components/RouteViewer.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react'
+import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
+import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
 import PlanEditor from './organisms/PlanEditor'
+import { useDirections } from 'hooks/useDirections'
+import { useSelectSpots } from 'hooks/useSelectSpots'
+import { SpotEvent } from 'contexts/SelectedPlacesProvider'
+import { SelectedPrefectureContext } from 'contexts/SelectedPrefectureProvider'
 
 const RouteViewer = () => {
+  const directionService = useDirections()
+  const selected = React.useContext(SelectedPrefectureContext)
+  const [events] = useSelectSpots()
+
+  const handleOptimize = async () => {
+    const result = await directionService.search(
+      selected.home.,
+      events
+        .filter(
+          (event): event is SpotEvent => event.extendedProps.type === 'spot'
+        )
+        .map((spot) => spot.extendedProps.placeId)
+    )
+    console.log(result)
+  }
   return (
     <Container
       maxWidth="md"
@@ -13,9 +34,17 @@ const RouteViewer = () => {
         flexFlow: 'column',
         height: '100%',
       }}>
-      <Typography variant="h4" component="h2">
-        Your travel plan
-      </Typography>
+      <Stack direction="row" alignItems="center" justifyContent="space-between">
+        <Typography variant="h4" component="h2">
+          Your travel plan
+        </Typography>
+        <Button
+          disabled={events.length < 2}
+          variant="contained"
+          onClick={handleOptimize}>
+          Optimize
+        </Button>
+      </Stack>
       <PlanEditor />
     </Container>
   )
--- src/hooks/useDirections.ts
+++ src/hooks/useDirections.ts
@@ -1,31 +1,34 @@
 import * as React from 'react'
 
 import { DirectionServiceContext } from 'contexts/DirectionServiceProvider'
-import { Spot } from 'contexts/SelectedPlacesProvider'
 
 export const useDirections = () => {
   const direction = React.useContext(DirectionServiceContext)
 
+  /**
+   * 入力されたスポットの一覧について、最適なルートを見つける
+   * @param placeIds PlaceID のリスト
+   */
   const search = React.useCallback(
-    async (places: Array<Spot>) => {
+    async (homeId: string, waypoints: Array<string>) => {
       if (direction === null) {
         throw Error('JS Google Map api is not loaded')
       }
-      if (places.length < 2) {
+      if (waypoints.length < 2) {
         console.error('the minimum length is 2')
       }
-      const cloned = Array.from(places)
 
-      const origin = { placeId: cloned.shift()?.placeId }
-      const destination = { placeId: cloned.pop()?.placeId }
-      const waypoints = cloned.map((place) => ({
-        location: { placeId: place.placeId },
-      }))
+      const origin = { placeId: homeId }
+      const destination = { placeId: homeId }
+
       const result = await direction.route(
         {
           origin,
           destination,
-          waypoints,
+          waypoints: waypoints.map((placeId) => ({
+            location: { placeId: placeId },
+          })),
+          optimizeWaypoints: true,
           region: 'JP',
           travelMode: google.maps.TravelMode.DRIVING,
         },
