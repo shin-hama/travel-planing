2c1d6366737ee9aabdabfb4e9d30081a60b3bb99
shin-hama <htysivista@gmail.com>
2022-03-16 07:02:13 +0900

    WIP: Add date picker on plan config

--- src/components/pages/PlanConfig.tsx
+++ src/components/pages/PlanConfig.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
 import Stack from '@mui/material/Stack'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import { useForm, Controller } from 'react-hook-form'
+import DateAdapter from '@mui/lab/AdapterDayjs'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import MobileDatePicker from '@mui/lab/MobileDatePicker'
+import { useForm, Controller, SubmitHandler } from 'react-hook-form'
+import dayjs from 'dayjs'
 
 import SelectPrefectureDialog, {
   Props,
@@ -15,7 +20,9 @@ import { usePlanEvents } from 'hooks/usePlanEvents'
 import { usePlan } from 'hooks/usePlan'
 import { useUnsplash } from 'hooks/useUnsplash'
 
-type PlanDTO = Pick<Plan, 'title' | 'start' | 'home' | 'destination'>
+type PlanDTO = Pick<Plan, 'title' | 'home' | 'destination'> & {
+  start: dayjs.Dayjs
+}
 
 const PrefectureSelector = () => {
   const { register, control, handleSubmit } = useForm<PlanDTO>()
@@ -40,7 +47,8 @@ const PrefectureSelector = () => {
     }
   }
 
-  const handleCreatePlan = async (planDTO: PlanDTO) => {
+  const handleCreatePlan: SubmitHandler<PlanDTO> = async (planDTO: PlanDTO) => {
+    console.log(planDTO)
     if (!planDTO?.home || !planDTO?.destination) {
       alert('please select home and destination')
       return
@@ -51,8 +59,8 @@ const PrefectureSelector = () => {
 
     const newPlan: Parameters<typeof createPlan>[number] = {
       title: planDTO.title,
-      start: new Date(),
-      end: new Date(),
+      start: planDTO.start.toDate(),
+      end: planDTO.start.toDate(),
       thumbnail: photo.urls.regular,
       home: planDTO.home,
       destination: planDTO.destination,
@@ -63,64 +71,78 @@ const PrefectureSelector = () => {
   }
 
   return (
-    <>
-      <Box
-        sx={{
-          mt: 2,
-          ml: 2,
-        }}>
-        <Typography variant="h5">旅程を作成する</Typography>
-      </Box>
-      <form
-        style={{ width: '100%' }}
-        onSubmit={handleSubmit(handleCreatePlan, () => {
-          console.log('invalid')
-        })}>
-        <Stack alignItems="center" spacing={2} sx={{ pt: 3, px: 2 }}>
-          <Box width="100%">
-            <TextField
-              fullWidth
-              label="プラン名"
-              variant="outlined"
-              defaultValue={'Travel Plan'}
-              {...register('title')}
-            />
-          </Box>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <Typography>出発地:</Typography>
+    <LocalizationProvider dateAdapter={DateAdapter}>
+      <Container maxWidth="xs">
+        <Box
+          sx={{
+            mt: 2,
+            ml: 2,
+          }}>
+          <Typography variant="h5">旅程を作成する</Typography>
+        </Box>
+        <form
+          style={{ width: '100%' }}
+          onSubmit={handleSubmit(handleCreatePlan, () => {
+            console.log('invalid')
+          })}>
+          <Stack alignItems="center" spacing={2} sx={{ pt: 3, px: 2 }}>
+            <Box width="100%">
+              <TextField
+                fullWidth
+                label="プラン名"
+                variant="outlined"
+                defaultValue={'Travel Plan'}
+                {...register('title')}
+              />
+            </Box>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Typography>出発地:</Typography>
+              <Controller
+                control={control}
+                name="home"
+                render={({ field }) => (
+                  <Button
+                    variant="outlined"
+                    onClick={async () => field.onChange(await handleClick())}>
+                    {field.value?.name || 'Select'}
+                  </Button>
+                )}
+              />
+            </Stack>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Typography>目的地:</Typography>
+              <Controller
+                control={control}
+                name="destination"
+                render={({ field }) => (
+                  <Button
+                    variant="outlined"
+                    onClick={async () => field.onChange(await handleClick())}>
+                    {field.value?.name || 'Select'}
+                  </Button>
+                )}
+              />
+            </Stack>
             <Controller
               control={control}
-              name="home"
+              name="start"
               render={({ field }) => (
-                <Button
-                  variant="outlined"
-                  onClick={async () => field.onChange(await handleClick())}>
-                  {field.value?.name || 'Select'}
-                </Button>
-              )}
-            />
+                <MobileDatePicker
+                  label="出発日"
+                  inputFormat="YYYY/MM/DD"
+                  value={field.value}
+                  onChange={field.onChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              )}></Controller>
+            <Button variant="contained" type="submit">
+              {"Let's Start Planning"}
+            </Button>
           </Stack>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <Typography>目的地:</Typography>
-            <Controller
-              control={control}
-              name="destination"
-              render={({ field }) => (
-                <Button
-                  variant="outlined"
-                  onClick={async () => field.onChange(await handleClick())}>
-                  {field.value?.name || 'Select'}
-                </Button>
-              )}
-            />
-          </Stack>
-          <Button variant="contained" type="submit">
-            Let's Start Planning
-          </Button>
-        </Stack>
-      </form>
-      <SelectPrefectureDialog {...openDialog} />
-    </>
+        </form>
+        <SelectPrefectureDialog {...openDialog} />
+      </Container>
+    </LocalizationProvider>
   )
 }
 
