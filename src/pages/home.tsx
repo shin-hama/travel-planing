b142abc54defdb599d814737cc83b7608fc1f2f6
shin-hama <htysivista@gmail.com>
2022-03-28 14:58:45 +0900

    FEAT: Update layout of static landing page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,41 @@
 import * as React from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 
-const Home = () => {
+type Props = {
+  image: string // The path of image
+}
+const Home: React.FC<Props> = ({ image }) => {
   return (
-    <>
-      Hello SSG
-      <Link href="/new">create plan</Link>
-    </>
+    <Grid container direction={['column-reverse', 'column-reverse', 'row']}>
+      <Grid item md={4} xs={12}>
+        <Stack alignItems="center" spacing={4} sx={{ mx: 4 }}>
+          <Typography variant="h3">キャッチコピー</Typography>
+          <Typography variant="h5">
+            Short description, Short description, Short description, Short
+            description, Short description, Short description, Short
+            description, Short description, Short description, Short
+            description, Short description, Short description,
+          </Typography>
+          <Link href="/new" passHref>
+            <Button variant="contained">プランを作成する</Button>
+          </Link>
+        </Stack>
+      </Grid>
+      <Grid item md={8} xs={12}>
+        <Image src={image} width="100%" height={100} alt="hero image" />
+      </Grid>
+    </Grid>
   )
 }
 
 export const getStaticProps = () => {
   return {
-    props: {},
+    props: { image: '/' },
   }
 }
 
