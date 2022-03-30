86fd8c1628cc42e93987174bce0358e4e4184328
shin-hama <htysivista@gmail.com>
2022-03-30 19:54:00 +0900

    FIX: Wrap Layout component on login and signup page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,20 +3,19 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import Link from 'next/link'
 
-import Header from 'components/modules/Header'
+import Layout from 'components/layouts/Layout'
 import LoginForm from 'components/modules/LoginForm'
 
 const Login = () => {
   return (
-    <>
-      <Header />
+    <Layout>
       <Stack alignItems="center" spacing={4} sx={{ my: 4, width: '100%' }}>
         <LoginForm />
         <Typography>
           登録がまだの方は<Link href="signup">こちら</Link>
         </Typography>
       </Stack>
-    </>
+    </Layout>
   )
 }
 
