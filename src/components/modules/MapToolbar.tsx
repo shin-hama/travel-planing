68fa7fe27513348d3c860fc81aaf0294cbce3861
shin-hama <htysivista@gmail.com>
2022-04-28 10:19:10 +0900

    FEAT: Use tab on map toolbar

--- src/components/modules/MapToolbar.tsx
+++ src/components/modules/MapToolbar.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react'
-import Badge from '@mui/material/Badge'
-import Stack from '@mui/material/Stack'
+import { styled } from '@mui/system'
+import SvgIcon from '@mui/material/SvgIcon'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import Typography from '@mui/material/Typography'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faCalendarWeek,
   faEllipsis,
-  faLocationDot,
+  faMapLocationDot,
+  faSuitcase,
 } from '@fortawesome/free-solid-svg-icons'
 
-import LabeledIconButton from 'components/elements/LabeledIconButton'
 import SpotsCandidates from 'components/modules/SpotsCandidates'
 import ScheduleViewer from 'components/layouts/ScheduleViewer'
 import { useTravelPlan } from 'hooks/useTravelPlan'
@@ -15,10 +19,19 @@ import PlanMenu from './PlanMenu'
 
 type Drawers = 'spots' | 'schedule'
 
+const MyTab = styled(Tab)`
+  padding: 0;
+`
+
 const MapToolbar = () => {
   const [plan] = useTravelPlan()
   const [open, setOpen] = React.useState<Drawers | null>(null)
   const [menuAnchor, setMenuAnchor] = React.useState<null | HTMLElement>(null)
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
 
   const handleOpen = (mode: Drawers) => () => {
     setOpen(mode)
@@ -29,7 +42,45 @@ const MapToolbar = () => {
 
   return (
     <>
-      <Stack
+      <Tabs
+        value={value}
+        variant="fullWidth"
+        onChange={handleChange}
+        sx={{ p: 0 }}>
+        <MyTab
+          icon={
+            <SvgIcon>
+              <FontAwesomeIcon icon={faSuitcase} />
+            </SvgIcon>
+          }
+          label={<Typography variant="caption">プラン情報</Typography>}
+        />
+        <MyTab
+          icon={
+            <SvgIcon>
+              <FontAwesomeIcon icon={faMapLocationDot} />
+            </SvgIcon>
+          }
+          label={<Typography variant="caption">マップ</Typography>}
+        />
+        <MyTab
+          icon={
+            <SvgIcon>
+              <FontAwesomeIcon icon={faCalendarWeek} />
+            </SvgIcon>
+          }
+          label={<Typography variant="caption">スケジュール</Typography>}
+        />
+        <MyTab
+          icon={
+            <SvgIcon>
+              <FontAwesomeIcon icon={faEllipsis} />
+            </SvgIcon>
+          }
+          label={<Typography variant="caption">設定</Typography>}
+        />
+      </Tabs>
+      {/* <Stack
         direction="row"
         justifyContent="space-around"
         alignItems="baseline">
@@ -50,7 +101,7 @@ const MapToolbar = () => {
           icon={faEllipsis}
           label={'設定'}
         />
-      </Stack>
+      </Stack> */}
       <SpotsCandidates
         open={open === 'spots'}
         spots={plan?.waypoints || []}
