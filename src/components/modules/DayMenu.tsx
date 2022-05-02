2b133a5b13ad0666b394703387e9a38ad99f0c53
shin-hama <htysivista@gmail.com>
2022-05-03 08:01:39 +0900

    FEAT: Add day column handler

---
+++ src/components/modules/DayMenu.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import Menu, { MenuProps } from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+
+import { useConfirm } from 'hooks/useConfirm'
+
+type Props = MenuProps & {
+  onDelete?: () => void
+}
+const DayMenu: React.FC<Props> = ({ onDelete, ...props }) => {
+  const confirm = useConfirm()
+
+  const handleDelete = async () => {
+    try {
+      await confirm({
+        title: 'CAUTION!!',
+        description: '日付内のデータが削除されます。よろしいですか?',
+      })
+      onDelete?.()
+    } finally {
+      props.onClose?.({}, 'backdropClick')
+    }
+  }
+
+  return (
+    <Menu
+      id="day-column-menu"
+      {...props}
+      MenuListProps={{
+        'aria-labelledby': 'basic-button',
+      }}>
+      <MenuItem onClick={handleDelete}>Delete Day</MenuItem>
+    </Menu>
+  )
+}
+
+export default DayMenu
--- src/components/modules/ListScheduler.tsx
+++ src/components/modules/ListScheduler.tsx
@@ -1,113 +1,76 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
 import Stack from '@mui/material/Stack'
+import SvgIcon from '@mui/material/SvgIcon'
 import Typography from '@mui/material/Typography'
-import dayjs from 'dayjs'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisVertical, faPlus } from '@fortawesome/free-solid-svg-icons'
 
-import { Plan, SpotEvent } from 'contexts/CurrentPlanProvider'
 import { useScheduleEvents } from 'hooks/useScheduleEvents'
-import { useWaypoints } from 'hooks/useWaypoints'
-import { PlanAPI } from 'hooks/useTravelPlan'
 import SpotCard from './SpotCard'
+import DayMenu from './DayMenu'
+import { useList } from 'react-use'
 
-// useTravelPlan() が何度も再読み込みされるのを防ぐために props で受け取る
-type Props = {
-  plan: Plan
-  planApi: PlanAPI
-}
-const ListScheduler: React.FC<Props> = ({ plan, planApi }) => {
-  const [events, eventsApi] = useScheduleEvents()
-  const [waypoints, waypointsApi] = useWaypoints()
+const ListScheduler: React.FC = () => {
+  const [events] = useScheduleEvents()
+  const [days, setDays] = useList([1])
+  const [target, setTarget] = React.useState(0)
+  const [anchor, setAnchor] = React.useState<null | HTMLElement>(null)
 
-  const [range, setRange] = React.useState<{
-    start: Date
-    end: Date
-  }>({
-    start: new Date(),
-    end: new Date(),
-  })
-
-  const handleEventsSet = (_events: EventApi[]) => {
-    if (_events.length === 0) {
-      return
-    }
-
-    const days = _events.map((e) => dayjs(e.start))
-    const sorted = days.sort((a, b) => a.diff(b))
-    const first = sorted[0]
-    const last = sorted[sorted.length - 1]
-    // Not update visibleRange if range is not changed
-    if (
-      first.date() !== range.start.getDate() ||
-      last.date() !== range.end.getDate()
-    ) {
-      setRange({
-        start: first.toDate(),
-        end: last.toDate(),
-      })
-    }
+  const handleAddDay = () => {
+    setDays.push(days.length + 1)
   }
 
-  const handleEventDrop = async (e: EventDropArg) => {
-    // 画面上で元の位置に戻らないようとりあえず Event を更新する
-    const droppedEvent = eventsApi.get<SpotEvent>(
-      e.event.id,
-      e.event.extendedProps.type
-    )
-    if (!droppedEvent) {
-      throw new Error('Cannot find dropped event')
-    }
+  const handleRemoveDay = (value: number) => () => {
+    setDays.filter((day) => day !== value)
+    setTarget(0)
+  }
 
-    if (e.event.end && e.oldEvent.end) {
-      if (Math.abs(e.event.end.getDate() - e.oldEvent.end.getDate()) >= 1) {
-        if (!waypoints) {
-          return
-        }
-
-        console.log('Move day')
-        const prevSpots = events
-          ?.filter(
-            (event): event is SpotEvent =>
-              event.extendedProps.type === 'spot' &&
-              // 移動したイベントよりも前のイベントでフィルター
-              dayjs(e.event.start).diff(event.start) > 0
-          )
-          .sort((a, b) => dayjs(b.start).diff(a.start)) // 開始日の降順に並び替え
-
-        const prevIndex = waypoints?.findIndex(
-          (spot) => spot.id === prevSpots[0].id
-        )
-
-        waypointsApi.move(
-          droppedEvent.id,
-          prevIndex !== -1 ? prevIndex : 0 // 移動した先にイベントがない場合は、最初に挿入する
-        )
-      } else {
-        // 同じ日付内で移動した場合は、全てのイベントの開始時刻を同じだけずらす
-        console.log('move all')
-
-        planApi.update({
-          startTime: dayjs(plan.startTime)
-            .add(e.delta.milliseconds, 'millisecond')
-            .toDate(),
-        })
-      }
-    }
+  const handleOpenMenu = (value: number, anchor: HTMLElement) => {
+    setTarget(value)
+    setAnchor(anchor)
   }
 
   return (
     <>
       <Stack direction="row" spacing={4}>
-        <Stack spacing={2} width="400px">
-          <Stack direction="row" justifyContent="space-between">
-            <Typography>1st Day</Typography>
-            <Box>+</Box>
+        {days.map((day) => (
+          <Stack key={`day-${day}`} spacing={2} width="400px">
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="space-between">
+              <Typography>Day {day}</Typography>
+              <Box>
+                <IconButton onClick={handleAddDay}>
+                  <SvgIcon>
+                    <FontAwesomeIcon icon={faPlus} />
+                  </SvgIcon>
+                </IconButton>
+                <IconButton
+                  onClick={(e) => handleOpenMenu(day, e.currentTarget)}>
+                  <SvgIcon>
+                    <FontAwesomeIcon icon={faEllipsisVertical} />
+                  </SvgIcon>
+                </IconButton>
+              </Box>
+            </Stack>
+            {events.map((event) => (
+              <SpotCard key={event.id} spot={event} />
+            ))}
           </Stack>
-          {events.map((event) => (
-            <SpotCard key={event.id} spot={event} />
-          ))}
-        </Stack>
+        ))}
       </Stack>
+      <DayMenu
+        anchorEl={anchor}
+        open={Boolean(anchor)}
+        onClose={() => {
+          setTarget(0)
+          setAnchor(null)
+        }}
+        onDelete={handleRemoveDay(target)}
+      />
     </>
   )
 }
